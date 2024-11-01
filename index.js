08f1075d22d83be8bf6514a293c692337011a8f7
Sebattaz <seba.lostar@gmail.com>
2024-10-31 21:45:49 -0300

    Registro de eventos

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 app.listen(3000, console.log("Servidor levantado"));
 
-const {obtenerJoyas, obtenerJoyasFiltro} = require("./consultas");
+const { obtenerJoyas, obtenerJoyasFiltro } = require("./consultas");
 
-app.get("/joyas", async (req, res)=>{
-    try{
-        const listJoyas = req.query;
-        const joyas = await obtenerJoyas(listJoyas);
-        res.json(joyas);
-    }catch(err){
-        console.log("Error", err);
-        res.status(500).json({Error: "al obtener Joyas"});
-    }
+const logEventos = (req, res, next) => {
+  const fecha = new Date().toISOString();
+  const log = `[${fecha}] ${req.method} ${req.url}\n`;
+
+  console.log(log);
+
+  fs.appendFile("registro_eventos.log", log, (err) => {
+    if (err) console.error("Error al registrar evento en log", err);
+  });
+  next();
+};
+
+app.use(logEventos);
+
+app.get("/joyas", async (req, res) => {
+  try {
+    const listJoyas = req.query;
+    const joyas = await obtenerJoyas(listJoyas);
+    res.json(joyas);
+  } catch (err) {
+    console.log("Error", err);
+    res.status(500).json({ Error: "al obtener Joyas" });
+  }
 });
 
-app.get("/joyas/filtros", async (req, res)=>{
-    try{
-        const listFiltro = req.query;
-        const joyas = await obtenerJoyasFiltro(listFiltro);
-        res.json(joyas);
-    }catch(err){
-        console.log("Error",err);
-        res.status(500).json({Error: "al filtrar"});
-    }
+app.get("/joyas/filtros", async (req, res) => {
+  try {
+    const listFiltro = req.query;
+    const joyas = await obtenerJoyasFiltro(listFiltro);
+    res.json(joyas);
+  } catch (err) {
+    console.log("Error", err);
+    res.status(500).json({ Error: "al filtrar" });
+  }
 });
